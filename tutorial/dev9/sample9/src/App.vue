ea9b522e8b46ef75c89ba56c9ae3abaa7bd1aac1
shunsuke00 <nemu.shun@icloud.com>
2023-10-25 21:02:21 +0900

    [6.9]Vuetifyデザイン変更2

--- tutorial/dev9/sample9/src/App.vue
+++ tutorial/dev9/sample9/src/App.vue
@@ -2,5 +2,5 @@
   <h1>Sample9</h1>
   <v-btn>ボタン1</v-btn>
   <v-btn variant="outlined">ボタン2</v-btn>
-  <v-btn variant="tonal" color="orange-darken-3">ボタン3</v-btn>
+  <v-btn variant="tonal" color="orange-darken-3">ボタン4</v-btn>
 </template>
