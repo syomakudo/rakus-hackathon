a309aacfcaec63da7a5c062ca3b66188f80ec004
SyunsukeTooyama <syunsuke.t.2021@outlook.jp>
2023-11-10 19:24:49 +0900

    アイコンの実装

--- chatapp/src/main.js
+++ chatapp/src/main.js
@@ -3,5 +3,6 @@ import "./style.css"
 import App from "./App.vue"
 import router from "./router"
 import vuetify from "./plugins/vuetify"
+import '@mdi/font/css/materialdesignicons.css'
 
 createApp(App).use(vuetify).use(router).mount("#app")
