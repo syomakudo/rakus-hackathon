79d0f7883de713ffb7034f0721bf85eb93cc1ff2
shunsuke00 <nemu.shun@icloud.com>
2023-11-11 21:42:01 +0900

    ダイアログの追加

--- chatapp/src/components/Login.vue
+++ chatapp/src/components/Login.vue
@@ -20,12 +20,13 @@ const message = reactive({
 const inputUserName = ref("");
 // #endregion
 
+const dialog = ref(false);
 // #region browser event handler
 // 入室メッセージをクライアントに送信する
 const onEnter = () => {
   // ユーザー名が入力されているかチェック
   if (inputUserName.value == "") {
-    alert("ユーザー名を入力してください。");
+    dialog.value = true;
     return;
   }
 
@@ -51,7 +52,8 @@ const onEnter = () => {
       <input type="text" class="user-name-text" placeholder="お名前を入力してください" v-model="inputUserName" />
     </div>
     <v-btn
-      rounded 
+      rounded
+      flat
       height="48" 
       width="242" 
       color="#FF8200" 
@@ -60,6 +62,18 @@ const onEnter = () => {
      >
       入室する
     </v-btn>
+    <v-dialog
+      v-model="dialog"
+    >
+      <div class="Dialog">
+        <div class="dialogtext">
+            お名前を入力してください。
+        </div>
+          <v-btn rounded flat height="48" width="242" 
+            class="buttonDialog"
+            @click="dialog = false">閉じる</v-btn>
+      </div>
+    </v-dialog>
   </div>
   </v-app>
 </template>
@@ -115,5 +129,38 @@ const onEnter = () => {
   font-size: 14px;
   font-style: normal;
   font-weight: 700;
+}
+
+/*ダイアログの箱の設定と内部の共通の設定*/
+.Dialog {
+  height: 240px;
+  width: 324px;
+  border-radius: 20px;
+  background: #FFF;
+  box-shadow: 0px 4px 8px 0px rgba(0, 0, 0, 0.25);
+  font-family: Inter;
+  font-style: normal;
+  font-size: 14px;
+  text-align: center;
+}
+/*ダイアログ内のテキストの設定 */
+.dialogtext {
+  color: #000;
+  font-weight: 400;
+  line-height: 22px; /* 157.143% */
+  letter-spacing: -0.408px;
+  margin-top: 65px;
+  margin-bottom: 71px;
+}
+/*ダイアログ内のボタンの設定 */
+.buttonDialog {
+  border: 1px solid #FF8200;
+  color: #FF8200;
+  padding: 3px 16px 2px 17px;
+  justify-content: center;
+  align-items: center;
+  font-weight: 700;
+  line-height: 22px; /* 157.143% */
+  letter-spacing: -0.408px;
 }
 </style>
